e4fbe29cee0f7eb88f2bcafa948bfe62316323e7
Sander Halvorsen <sandhal@stud.ntnu.no>
2025-02-20 17:13:39 +0100

    idk :)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: undefined, // ❌ REMOVE "export" mode if it exists
 };
 
 export default nextConfig;
