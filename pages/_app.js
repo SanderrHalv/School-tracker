723bdd6f5ee4d3401bb7f2890e28fe0242264a9e
Sander Halvorsen <sandhal@stud.ntnu.no>
2025-02-20 16:05:00 +0100

    appppp

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import "../styles/globals.css";
-import { DarkModeProvider } from "../context/DarkModeContext";
+import { DarkModeProvider } from "../context/DarkModeContext"; // ✅ Import DarkModeProvider
 import AuthProvider from "../components/AuthProvider";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <DarkModeProvider>
-      <AuthProvider>
         <Component {...pageProps} />
-      </AuthProvider>
-    </DarkModeProvider>
   );
 }
